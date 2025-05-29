e41d651fddf63374819313b49c84010c934fe5fd
Jeremy <jeremy.garcia@vtsd.com>
2025-05-29 12:27:28 -0400

    lab03

---
+++ Lab03/Lab03.java
@@ -0,0 +1,17 @@
+package Lab03;
+
+public class Lab03 {
+        public static void main(String[] args) {
+            System.out.println("lab03, 100 Point Version\n");
+
+            int milliseconds = 1000;
+            int second = milliseconds / 1000;
+            int second = 10000;
+            int hours = second / 3600000;
+
+
+            System.out.println("starting seconds: \t" + second);
+            System.out.println("Hours: \t\t\t\t" + hours);
+        }
+
+}
